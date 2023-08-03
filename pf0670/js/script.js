8416be5acf1912eb1a809a0c587a55f03acf2e76
pbrnx <pedrobarone2709@gmail.com>
2023-08-03 20:29:46 -0300

    leitura de arrays

---
+++ pf0670/js/script.js
@@ -0,0 +1,35 @@
+//console.log("Hello World");
+/*
+
+//leituras de arrays
+
+//declaração de array de nomes de alunos com 5 nomes
+let aluno = ["Pedro","João", "Luis", "Mauricio", "Vitor"];
+//logando no console
+console.log(aluno);
+//logando um elemento do array
+console.log(`O primeiro Aluno é ${aluno[0]}`);
+//ler o array com for each
+aluno.forEach( al =>{
+    //imprimir cada item do array
+    console.log(`${al} está aprendendo JS`);
+});
+//quebra de linha
+console.log("\n");
+
+//ler com for
+for(let i=0; i < aluno.length ;++i){
+    if(aluno[i] === "Pedro"){
+        break;
+    }else{
+        console.log("Nome do aluno " + aluno[i]);
+    }
+}
+
+//ler o array com for in
+for (let i in aluno) {
+    console.log("Esse é o aluno : " + aluno[i])
+}  
+*/
+
+
