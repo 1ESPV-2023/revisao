dfdd9bf7acf177e34b3a1479774b86b0915543fb
TonyWillianFIAP <rm550667@fiap.com.br>
2023-08-10 20:00:13 -0300

    aa

---
+++ rm550667/js/tarefa.js
@@ -0,0 +1,9 @@
+//Criando a lista de tarefas
+let listaTarefasArray = [];
+
+//Recuperando o botão de envio
+const botaoAddTarefa =  document.querySelector('btnAddTarefa')
+botaoAddTarefa.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    
+});
