2aae665f1392d4502a157c9265da79811d943e75
TonyWillianFIAP <rm550667@fiap.com.br>
2023-08-10 20:44:45 -0300

    Removendo da array também agora

--- rm550667/js/tarefa.js
+++ rm550667/js/tarefa.js
@@ -18,6 +18,22 @@ botaoAddTarefa.addEventListener("click",(evt)=>{
 
     listaTarefasUL.appendChild(li);
 
+    let botaoExcluir = document.createElement('button');
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+    
+    botaoExcluir.addEventListener ("click", (evt)=>{
+        evt.preventDefault();
+        let conteudoDaLi = evt.target.parentNode.textContent.split(" x ");
+        let indiceDaTarefa = listaTarefasArray.indexOf(conteudoDaLi[0]);
+        listaTarefasArray.splice(indiceDaTarefa,1);
+
+        evt.target.parentNode.remove();
+
+    });
+
     console.log(listaTarefasArray);
     inputTarefa.value = "";
 });
