14464d39464babd0b74db3113c1d6b0574ed853e
Luan Nijou <RM98920@fiap.com.br>
2023-10-22 14:32:31 -0300

    Update

--- RM98920/js/script.js
+++ RM98920/js/script.js
@@ -2,8 +2,8 @@ let listaTarefasArray = [];
 
 const botaoAddTarefa = document.querySelector('#btnAddTarefa');
 const botaoExcluirTarefa = document.querySelector('#btnExcluirTarefa');
-
-
+const btnOrganizar = document.querySelector('#btnOrganizar');
+const tabelaTarefas = document.querySelector('#lista-tarefas');
 
 botaoAddTarefa.addEventListener("click",(evt)=>{
     evt.preventDefault();
@@ -12,68 +12,104 @@ botaoAddTarefa.addEventListener("click",(evt)=>{
     const inputDescricaoTarefa = document.getElementById("idDescTarefa");
     const inputDepartamentoTarefa = document.getElementById("idDpTarefa");
     const inputimportanciaTarefa = document.getElementById("idImporTarefa");
-    const inputDataTarefa = document.getElementById("idDtTarefa");
     
-
     const tarefaObj = {
         nome: inputNomeTarefa.value,
-        descricao:inputDescricaoTarefa.value,
-        departamento:inputDepartamentoTarefa.value,
-        importancia:inputimportanciaTarefa.value,
-        data:inputDataTarefa.value,
+        descricao: inputDescricaoTarefa.value,
+        departamento: inputDepartamentoTarefa.value,
+        importancia: inputimportanciaTarefa.value,
     }
-
     listaTarefasArray.push(tarefaObj);
 
-    const listaTarefasUL = document.querySelector('#lista-tarefas');
-    let tr = document.createElement('tr');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    let td5 = document.createElement('td');
-    let td6 = document.createElement('td');
-    let check = document.createElement('input');
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
+    const td6 = document.createElement('td');
+    const check = document.createElement('input');
+    const data = document.createElement('input');
+
     check.type = 'checkbox';
-    check.id = 'checkbox1';
+    check.className = "checkbox-tarefa";
+    check.id = `checkbox-${listaTarefasArray.length}`;
+
+    data.type = 'date';
 
     td1.textContent = tarefaObj.nome;
     td2.textContent = tarefaObj.descricao;
     td3.textContent = tarefaObj.departamento;
     td4.textContent = tarefaObj.importancia;
-    td5.textContent = tarefaObj.data;
 
-
-    listaTarefasUL.appendChild(tr);
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
     tr.appendChild(td5);
+    td5.appendChild(data);
     tr.appendChild(td6); 
     td6.appendChild(check);
 
+    tabelaTarefas.appendChild(tr);
+
     console.log(check.value);
     console.log(listaTarefasArray);
-   
 });
 
 botaoExcluirTarefa.addEventListener("click", (evt) => {
     evt.preventDefault();
 
-    // Percorra a lista de tarefas em ordem reversa para evitar problemas de índice ao remover elementos
-    for (let i = listaTarefasArray.length - 1; i >= 0; i--) {
-        const tarefa = listaTarefasArray[i];
+    const checkboxes = [...document.querySelectorAll('.checkbox-tarefa')];
 
-        // Verifique se a caixa de seleção correspondente está marcada
-        const checkbox = document.querySelector(`#checkbox1-${i}`);
-        
-        if (checkbox.checked) { // Verifique se o checkbox está marcado usando checkbox.checked
-            // Remova a linha da tabela e o objeto da lista de tarefas
-            listaTarefasArray.splice(i, 1);
+    checkboxes.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            listaTarefasArray.splice(index, 1);
             const trToRemove = checkbox.closest('tr');
             trToRemove.remove();
         }
-    }
+    });
 });
 
+btnOrganizar.addEventListener('click', () => {
+    listaTarefasArray.sort((a, b) => b.importancia - a.importancia);
+    
+    // Limpar a tabela, exceto a linha de cabeçalho
+    while (tabelaTarefas.children.length > 1) {
+        tabelaTarefas.removeChild(tabelaTarefas.lastChild);
+    }
+
+    listaTarefasArray.forEach(tarefa => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+        const td4 = document.createElement('td');
+        const td5 = document.createElement('td');
+        const td6 = document.createElement('td');
+        const check = document.createElement('input');
+        const data = document.createElement('input');
+
+        check.type = 'checkbox';
+        check.className = "checkbox-tarefa";
+        check.id = `checkbox-${listaTarefasArray.length}`;
+
+        data.type = 'date';
+
+        td1.textContent = tarefa.nome;
+        td2.textContent = tarefa.descricao;
+        td3.textContent = tarefa.departamento;
+        td4.textContent = tarefa.importancia;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        td5.appendChild(data);
+        tr.appendChild(td6); 
+        td6.appendChild(check);
+
+        tabelaTarefas.appendChild(tr);
+    });
+});
